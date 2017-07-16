e072b1bd5c96fedd5300c786e5ca7cd7c0c9e7b1
Victor Lee <myleevictor@gmail.com>
2017-07-16 22:20:00 +0800

    import AVKit and instantiate AVCaptureSession

--- SmartCameraLBTA/ViewController.swift
+++ SmartCameraLBTA/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        // AVCaptureDeviceInput may throw error if device has no camera so use try? if don't want to use Do Try Catch let
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
