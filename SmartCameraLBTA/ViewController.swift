ff07d8d79b9d97bac751da8bcb0adfac66450a9c
Victor Lee <myleevictor@gmail.com>
2017-07-16 22:28:37 +0800

    set previewLayer and it's frame to view's frame

--- SmartCameraLBTA/ViewController.swift
+++ SmartCameraLBTA/ViewController.swift
@@ -15,16 +15,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let captureSession = AVCaptureSession()
-        
-        
+        captureSession.sessionPreset = .photo // give it a more cropped look.  Not compulsory.
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
         
         // AVCaptureDeviceInput may throw error if device has no camera so use try? if don't want to use Do Try Catch let
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        
         captureSession.addInput(input)
-        
         captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        // Add Frame
+        previewLayer.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
